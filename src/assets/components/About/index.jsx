f239721b3d10c7c2854a3540da37f7544799b2dd
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-04-18 13:30:32 +0100

    Components added

---
+++ src/assets/components/About/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About () {
+  return (
+    <div>
+    <div className="aboutMe">
+    <h5>About Me</h5> 
+    
+     
+    </div>
+
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/assets/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Footer Component
+function Footer() {
+    return (
+        <footer className="footer">
+            <div>Made with ❤️️ by Alana &copy; 2023 Alana Barrett-Frew</div>
+        </footer>
+    );
+}
+
+export default Footer;
