eb90a5ec91480795a98e1d7a4d8324e434397832
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-04-18 15:31:46 +0100

    app.js edited and navbar so it doesnt crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,46 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   )
-// }
-
-// export default App
-
 import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Outlet,
+  Route,
+} from "react-router-dom";
 import { Home, Contact, ProjectGallery, Blog, Freelance } from "./pages/index";
-import Navbar from "./components/Header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
 import "./index.css";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Home" element={<Home />} />
+const App = () => {
+  const router = createHashRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route path="/" index element={<Home />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Blog" element={<Blog />} />
         <Route path="/ProjectGallery" element={<ProjectGallery />} />
         <Route path="/Freelance" element={<Freelance />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      </Route>
+    )
   );
-}
+  return <RouterProvider router={router} />;
+};
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+};
 
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,49 +1,48 @@
 import React from "react";
 import favicon from "../../assets/favicon/apple-touch-icon.png";
+import '../../index.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
-function Navbar() {
+function Header() {
   return (
-    <nav className="navigation container-fluid navbar navbar-expand-lg bg-body-tertiary">
-      <a className="navbar-brand" href="./Home">
-        <img
-          src={favicon}
-          alt="Logo"
-          width="50"
-          height="50"
-          className="d-inline-block align-text-top"
-        />
-      </a>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <div className="navbar-nav me-auto mb-2 mb-lg-0">
-          <div className="nav-item">
-            <a className="nav-link active" aria-current="page" href="./Home">
-              Home
-            </a>
-            <a
-              className="nav-link active"
-              aria-current="page"
-              href="./ProjectGallery"
-            >
-              Project Gallery
-            </a>
-            <a
-              className="nav-link active"
-              aria-current="page"
-              href="./Freelance"
-            >
-              Freelance Work
-            </a>
-            <a className="nav-link active" aria-current="page" href="./Blog">
-              Blog
-            </a>
-            <a className="nav-link active" aria-current="page" href="./Contact">
-              Contact Me
-            </a>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Container fluid className='p-0'>
+<Navbar expand="navigation navbar lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand >
+              <div className='logo-container'>
+                <div className='logo-text logo-mobile'>
+                </div>
+                <div className='logo-holder'>
+                  <img src={favicon} alt='logo' width="60px" />
+                </div>
+              </div>
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to='/ProjectGallery'>
+                <Nav.Link >Project Gallery</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/Freelance">
+                <Nav.Link >Freelance Work</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/Blog">
+                <Nav.Link>Blog</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/Contact">
+                <Nav.Link>Contact Me</Nav.Link>
+                </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Container>
   );
 }
 
-export default Navbar;
+export default Header;
