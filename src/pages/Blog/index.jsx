7aba6482e2c74c7d4b411656d3dbad3f1ee3f20f
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-14 11:29:00 +0100

    link added to sign up for newsletter

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import BlogPost from "../../components/Blog";
-import blog from "../../blog.json"
-import "./blog.css"
+import blog from "../../blog.json";
+import "./blog.css";
 function Blog() {
   return (
     <div className="page-background">
       <div className="picture-section">
         <div className="picture">
           <h1>Blog</h1>
-         </div>
         </div>
-     <div>
-      <br/>
-      <br/>
+      </div>
+      <div>
+        <br />
+        <br />
+        <a href="https://newsletter-signup-ntrf.onrender.com/">
+          <button class="btn btn-lg btn-primary btn-block" type="button">
+            Signup for new content
+          </button>
+        </a>
         <p className="blogText">
           Welcome to my blog space. Join me on my journey as I take a massive
           leap into the unknown with a career change from Teacher to Front End
@@ -25,7 +30,7 @@ function Blog() {
           <BlogPost key={blogpost.id} blogpost={blogpost} />
         ))}
       </div>
-   </div>
+    </div>
   );
 }
 
