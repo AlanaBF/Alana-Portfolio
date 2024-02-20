7a39f77250a355767642a798bfe27867e2a90f32
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2024-02-20 09:04:44 +0000

    bug fixing

--- src/components/Avatar/UnityAvatar.jsx
+++ src/components/Avatar/UnityAvatar.jsx
@@ -5,10 +5,10 @@ function UnityAvatar() {
   const { unityProvider } = useUnityContext({
 
 
-    loaderUrl: "Build/Avatar.loader.js",
-    dataUrl: "Build/Avatar.data.gz",
-    frameworkUrl: "Build/Avatar.framework.js.gz",
-    codeUrl: "Build/Avatar.wasm.gz",
+    loaderUrl: "/Build/Avatar.loader.js",
+    dataUrl: "/Build/Avatar.data.gz",
+    frameworkUrl: "/Build/Avatar.framework.js.gz",
+    codeUrl: "/Build/Avatar.wasm.gz",
   });
 
   return <Unity className="unity-avatar" unityProvider={unityProvider} />;
