3d8529822c52d240179f495f1f42df897a8e2fa5
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2024-04-11 20:04:18 +0100

    new project added

--- src/components/GoogleSearch/index.jsx
+++ src/components/GoogleSearch/index.jsx
@@ -3,7 +3,7 @@ import '../../assets/styles/components.css'
 
 function GoogleSearch() {
     return (
-        <div>
+        <div className='googleContainer'>
             <h3 className="googleSearchText">Google Search</h3>
             <form className="googleSearch" method="GET" action="https://www.google.com/search" target="_blank">
                 <input className="googleTextEnter" name="q" type="text" />
