976b9680ac54895a8405eeb42468c80cc75b8306
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2024-07-08 21:03:10 +0100

    mobile edit added incase the modal doesnt show content

--- src/pages/Resources/PdfModal.jsx
+++ src/pages/Resources/PdfModal.jsx
@@ -3,13 +3,23 @@ import { Modal, Button } from "react-bootstrap";
 import "../../assets/styles/pages.css";
 
 function PdfModal({ show, onHide, title, pdfUrl }) {
+  // Simple check for mobile devices
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
   return (
-    <Modal show={show} onHide={onHide} >
+    <Modal show={show} onHide={onHide} size="lg">
       <Modal.Header closeButton>
         <Modal.Title>{title}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <embed src={pdfUrl} type="application/pdf" width="100%" height="600px" />
+        {isMobile ? (
+          <div>
+            <p>PDF preview is not available on mobile. Please download the file to view it.</p>
+            <Button style={{backgroundColor:"var(--dark-blue)", border:"none"}} href={pdfUrl} target="_blank">Download PDF</Button>
+          </div>
+        ) : (
+          <embed src={pdfUrl} type="application/pdf" width="100%" height="600px" />
+        )}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onHide}>Close</Button>
